443ca4ffe39fc462adf1fcd2851d16cbdb283c2f
LucasMCunha <90118707+LucasMCunha@users.noreply.github.com>
2023-04-10 16:15:15 -0300

    Add files via upload

---
+++ Lollapalooza/Lollapalooza/LollapaloozaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LollapaloozaApp.swift
+//  Lollapalooza
+//
+//  Created by Lucas Cunha on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LollapaloozaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
